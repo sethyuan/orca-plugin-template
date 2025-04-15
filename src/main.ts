2cd70a3b705455931e11961a8bfd72d68f421994
Seth <sethyuan@gmail.com>
2025-04-15 16:11:43 +0800

    feat: add l10n

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import { setupL10N, t } from "./libs/l10n";
+import zhCN from "./translations/zhCN";
+
 let pluginName: string;
 
 export async function load(_name: string) {
   pluginName = _name;
 
+  setupL10N(orca.state.locale, { "zh-CN": zhCN });
+
   // Your plugin code goes here.
+  console.log(t("your plugin code starts here"));
 
   console.log(`${pluginName} loaded.`);
 }
